284d1f2a77361f8305d38b500f4192233eedebdd
Ergun Polat <ergun.polat@sky.uk>
2020-02-29 16:44:35 +0000

    added game screen

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
-import  Header  from './components/Header';
+import Header from './components/Header';
 import StartGameScreen from './screens/StartGameScreen';
+import GameScreen from './screens/GameScreen';
 
 const App = () => {
+  const [userNumber, setUserNumber] = useState();
+
+  const startGameHander = selectedNumber => {
+    setUserNumber(selectedNumber);
+  };
+
+  let content = <StartGameScreen onStartGame={startGameHander} />;
+
+  if (userNumber) {
+    content = <GameScreen userChoice={userNumber} />;
+  }
+  
   return (
     <View style={styles.container}>
-      <Header title="Guess a number"/>
-      <StartGameScreen/>
+      <Header title='Guess a number' />
+      {content}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-  },
+    flex: 1
+  }
 });
 export default App;
